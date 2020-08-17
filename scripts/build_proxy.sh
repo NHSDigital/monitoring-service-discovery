75dffe7aeba968ee60d49a246aa8b298abf7415a
jalal <jalalhosseiny@gmail.com>
2020-08-17 14:25:13 +0100

    AMB-229 remove sandbox from build_proxy.sh script

--- scripts/build_proxy.sh
+++ scripts/build_proxy.sh
@@ -2,13 +2,9 @@
 
 set -o nounset errexit pipefail
 
-# Collect the API Proxy and Hosted Target (Sandbox server)
+# Collect the API Proxy
 # files into build/apiproxy/ and deploy to Apigee
 
 rm -rf build/proxies
-mkdir -p build/proxies/sandbox
 mkdir -p build/proxies/live
-cp -Rv proxies/sandbox/apiproxy build/proxies/sandbox
 cp -Rv proxies/live/apiproxy build/proxies/live
-mkdir -p build/proxies/sandbox/apiproxy/resources/hosted
-rsync -av --copy-links --exclude="node_modules" --filter=':- .gitignore' sandbox/ build/proxies/sandbox/apiproxy/resources/hosted
