623aa95c05b368aa7b63fa40f55793fb8dac0cd8
Henry Iwanejko <henry.iwanejko1@nhs.net>
2021-05-24 13:51:55 +0100

    APM-2213 Append endpoints instead of overriding them

--- proxies/live/apiproxy/resources/jsc/update-service-entries.js
+++ proxies/live/apiproxy/resources/jsc/update-service-entries.js
@@ -4,15 +4,17 @@ rawEntries = context.getVariable("service.entries") || {};
 entries = JSON.parse(rawEntries);
 
 serviceName = Object.keys(payload)[0];
+env = Object.keys(payload[serviceName])[0];
 
 if (!entries[serviceName]) {
   Object.assign(entries, payload);
 
 } else {
-  serviceEntries = entries[serviceName];
-  envEntries = payload[serviceName];
+  serviceEndpoints = entries[serviceName][env];
+  endpointEntries = payload[serviceName][env];
 
-  Object.assign(serviceEntries, envEntries);
+  endpoints = Array.from(new Set(serviceEndpoints.concat(endpointEntries)));
+  entries[serviceName][env] = endpoints
 }
 
 context.setVariable("service.entries", JSON.stringify(entries));
